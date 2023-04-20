8ac47c52f0e7113359ad3a6e9079bb21ae043bc4
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2023-04-20 16:20:19 +0100

    created today component

--- src/app/today/today.component.ts
+++ src/app/today/today.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ForecastService } from '../forecast.service';
 
+
 @Component({
   selector: 'app-today',
   templateUrl: './today.component.html',
@@ -8,12 +9,51 @@ import { ForecastService } from '../forecast.service';
 })
 export class TodayComponent implements OnInit {
 
+  timeline = [];
+  weatherNow: any;
+  currentTime = new Date();
+  location: any;
+
   constructor(private forecastService: ForecastService) { }
 
   ngOnInit(): void {
     this.forecastService.getWeatherForecast().subscribe(data => {
-      console.log(data);
-    })
+      this.getTodayForecast(data);
+    //  console.log("FORECAST SERVICE DATA", data);
+    });
+
   }
+
+  getTodayForecast(today:any) {
+    console.log("TODAY", today);
+    this.location = today.city;
+    for (const forecast of today.list.slice(0, 8)) {
+      //console.log("FORECAST DATA", forecast);
+      // this.timeline.push({
+      //   time: forecast.dt_txt,
+      //   temp: forecast.main.temp  
+      // });
+
+
+      const apiDate = new Date(forecast.dt_txt).getTime();
+
+      if(this.dateRange().start.getTime() <= apiDate && this.dateRange().to.getTime() >= apiDate) {
+        this.weatherNow = forecast;
+        console.log("WEATHER NOW",this.weatherNow);
+      }
+
+    }
+  }
+
+  dateRange(){
+    const start = new Date();
+    start.setHours(start.getHours() + (start.getTimezoneOffset() / 60));
+    const to = new Date(start);
+    to.setHours(to.getHours() + 2, to.getMinutes() + 59, to.getSeconds() + 59);
+
+    return { start, to };
+  }
+
+
 
 }
