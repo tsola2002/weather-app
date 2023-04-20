4ed1ff1f48f5255164255f9a1f4e7e1788251a03
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2023-04-20 14:07:44 +0100

    added functionality to pull in data from open weather api

--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
 
-  constructor() { }
+  //apiKey = '14fe280929b43808d00f4037173197b4';
+
+  constructor(private http:HttpClient) { }
+
+  getWeather(): Observable<any>{
+    // get information from weather API
+    return this.http.get('https://api.openweathermap.org/data/2.5/weather?q=lagos&appid=14fe280929b43808d00f4037173197b4&units=imperial');
+    //return this.http.get('https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${this.apiKey}');
+  }
 }
--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { WeatherService } from '../weather.service';
 
 @Component({
   selector: 'app-weather',
@@ -7,9 +8,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WeatherComponent implements OnInit {
 
-  constructor() { }
+  weatherData: any;
+  temperature: number = 0;
+  feelsLikeTemp: number = 0;
+  humidity: number = 0;
+  pressure: number = 0;
+  summary: string = '';
+  iconURL: string = '';
+
+  constructor(private weatherService: WeatherService) { }
 
   ngOnInit(): void {
-  }
+
+      
+    
+
+      this.weatherService.getWeather().subscribe({
+        next: (res) => {
+          console.log(res);
+          this.weatherData = res;
+          this.temperature = this.weatherData.main.temp;
+          this.feelsLikeTemp = this.weatherData.main.feels_like;
+          this.humidity = this.weatherData.main.humidity;
+          this.pressure = this.weatherData.main.pressure;
+          this.summary = this.weatherData.weather[0].main;
+          this.iconURL = 'http://openweathermap.org/img/wn/' + this.weatherData.weather[0].icon + '@2x.png';
+        },
+        error: (error) => console.log(error.message),
+
+        complete: () => console.info('API call completed')
+      })
+   }
+
+  //  getWeather() {
+  //   this.weatherService.getWeather(this.city).subscribe(data => {
+  //     this.weatherData = data;
+  //   });
+  //  }
 
 }
